df4eb5f618bdb44203e2bbdf913e985bb56c3c4a
Ellie Rutherford <erutherf@tcd.ie>
2021-12-09 22:19:12 +0000

    try use send/receive

--- Lab5/MPIMM.c
+++ Lab5/MPIMM.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
@@ -10,13 +12,13 @@ int main(int argc, char **argv) {
    double *a, *b, *c, *rowA, *colB, start, sum, *allC, sumdiag;
    int i, j, k;
    double matA[] = {1,4,8,6,3,5,8,10,12,2,4,3,7,1,8,9,10,11,3,4,5,2,8,1,7,6,9,8,6,7,2,2,1,3,8,9};
-   double matB[] = {8,6,3,4,5,2,1,3,8,3,1,5,5,1,3,5,4,5,7,6,9,2,4,6,6,4,2,3,4,5,8,9,7,1,2,3};
+   double matB[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
    n = atoi(argv[1]);
    MPI_Comm rowComm, colComm;
    MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD,&p);
    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
-   
+
    int sqrtp = (int)sqrt(p);
    int numOfSquares = p;
    int squareSize = n/sqrtp;
@@ -26,16 +28,31 @@ int main(int argc, char **argv) {
    c = malloc(squareSize*squareSize*sizeof(double));
    rowA = malloc(squareSize*n*sizeof(double));
    colB = malloc(squareSize*n*sizeof(double));
+   int currentProc;
+   for(int i=0;i<n*n;i+=n*squareSize){
+       for(int k=0;k<sqrtp*squareSize;k+=squareSize){
+           currentProc++;
+           for(int j=0;j<squareSize;j++){
+               MPI_Send(matA[i+(n*j)+k],squareSize,MPI_DOUBLE,currentProc,1,MPI_COMM_WORLD);
+           }
+       }
+   }
+   for(int i=0;i<p;i++){
+       MPI_Recv(a,squareSize,MPI_DOUBLE,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE)
+   }
+   //MPI_Type_create_resized(col, 0, 1*sizeof(double), &coltype);
+   //MPI_Type_commit(&coltype);
    /*for(i=0; i<squareSize*squareSize; i++) {
      a[i] = 1.;
      b[i] = 2.;
    }*/
    MPI_Scatter(matA, squareSize*squareSize, MPI_DOUBLE, a, squareSize*squareSize, MPI_DOUBLE,myrank,MPI_COMM_WORLD);
-   MPI_Scatter(matB, squareSize*squareSize, MPI_DOUBLE, b, squareSize*squareSize, MPI_DOUBLE,myrank,MPI_COMM_WORLD);
-   printf("A: rank is %d:\n",myrank);
+   //MPI_Scatter(matrix, part_size, coltype, mypart, part_size, coltype, 0, MPI_COMM_WORLD);
+   MPI_Scatter(matB, squareSize*squareSize, MPI_DOUBLE, b, squareSize*squareSize, MPI_DOUBLE, myrank, MPI_COMM_WORLD);
+   /*printf("A: rank is %d:\n",myrank);
    for(int i=0;i<squareSize*squareSize;i++){
        printf("%lf ", a[i]);
-   }
+   }*/
    printf("\n");
    printf("B: rank is %d:\n",myrank);
    for(int i=0;i<squareSize*squareSize;i++){
@@ -47,7 +64,7 @@ int main(int argc, char **argv) {
      start = MPI_Wtime();
 
    int colour = myrank/sqrtp;
-   printf("colour for processor %d is %d\n", myrank,colour);
+   //printf("colour for processor %d is %d\n", myrank,colour);
 
    MPI_Comm_split(MPI_COMM_WORLD, colour, myrank, &rowComm);
    MPI_Comm_split(MPI_COMM_WORLD,myrank%sqrtp,myrank,&colComm);
@@ -60,25 +77,33 @@ int main(int argc, char **argv) {
        MPI_Gather(b, squareSize*squareSize, MPI_DOUBLE, colB, (n*n)/p, MPI_DOUBLE, i, colComm);
    }
 
-   /*for(int l=0;l<squareSize*n;l++){
-       printf("%lf ",rowA[l]);
+   for(int l=0;l<squareSize*n;l++){
+       printf("rank is %d, element %d of rowA is %lf \n",myrank,l,rowA[l]);
        //if(l=n*n-1){
          //printf("\n");
        //}
    }
-
    for(int l=0;l<squareSize*n;l++){
+       printf("rank is %d, element %d of colB is %lf \n",myrank,l,colB[l]);
+   }
+
+   /*for(int l=0;l<squareSize*n;l++){
       printf("%lf ",colB[l]);
    }*/
 
-   for(i=0; i<squareSize; i++)
+
+   for(i=0; i<squareSize; i++){
        for(j=0; j<squareSize; j++) {
            sum = 0.;
-           for(k=0; k<squareSize; k++)
-               sum += a[i*squareSize+k]*b[k*squareSize+j];
-           c[i*squareSize+j] = sum;
-        }
-   
+           for(int l=0;l<sqrtp;l++){
+               for(k=0; k<squareSize; k++){
+                   sum += rowA[i*squareSize+k+(l*squareSize*squareSize)]*colB[k*squareSize+j+(l*squareSize*squareSize)];
+               }
+           }
+           c[i*squareSize+j] += sum;
+       }
+    }
+
    free(rowA);
    free(colB);
    MPI_Barrier(MPI_COMM_WORLD);
