66577eb11393ada56d3c92aeb7f8ef844a4927b7
Ellie Rutherford <erutherf@tcd.ie>
2021-12-10 18:08:44 +0000

    working E2Egit add MPIMM.c

--- Lab5/MPIMM.c
+++ Lab5/MPIMM.c
@@ -5,14 +5,17 @@
 #include <float.h>
 #include <mpi.h>
 #include <math.h>
+#include <time.h>
+
+void PrintMatrix(double *matrixToPrint);
+void InitializeMatrix(int seed, double *matrix);
 
 int n, p;
 int main(int argc, char **argv) {
    int myn, myrank;
-   double *a, *b, *c, *rowA, *colB, start, sum, *allC, sumdiag;
+   double *a, *b, *c, *matA, *matB, *rowA, *colB, start, sum, *allC, sumdiag;
    int i, j, k;
-   double matA[] = {1,4,8,6,3,5,8,10,12,2,4,3,7,1,8,9,10,11,3,4,5,2,8,1,7,6,9,8,6,7,2,2,1,3,8,9};
-   double matB[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+   
    n = atoi(argv[1]);
    MPI_Comm rowComm, colComm;
    MPI_Init(&argc, &argv);
@@ -22,6 +25,19 @@ int main(int argc, char **argv) {
    int sqrtp = (int)sqrt(p);
    int numOfSquares = p;
    int squareSize = n/sqrtp;
+
+   if(myrank==0){
+       matA = malloc(n*n*sizeof(double));
+       matB = malloc(n*n*sizeof(double));
+
+       InitializeMatrix(1,matA);
+       sleep(1); // sleep so we get different random numbers in each matrix
+       InitializeMatrix(1,matB);
+       printf("Printing mat A: \n");
+       PrintMatrix(matA);
+       printf("Printing mat B: \n");
+       PrintMatrix(matB);
+   }
 
    a = malloc(squareSize*squareSize*sizeof(double));
    b = malloc(squareSize*squareSize*sizeof(double));
@@ -70,34 +86,15 @@ int main(int argc, char **argv) {
    }
 
    MPI_Wait(&recvRequestB, MPI_STATUS_IGNORE);
-   
-   //MPI_Scatter(matA, squareSize*squareSize, MPI_DOUBLE, a, squareSize*squareSize, MPI_DOUBLE,myrank,MPI_COMM_WORLD);
-   //MPI_Scatter(matrix, part_size, coltype, mypart, part_size, coltype, 0, MPI_COMM_WORLD);
-   //MPI_Scatter(matB, squareSize*squareSize, MPI_DOUBLE, b, squareSize*squareSize, MPI_DOUBLE, myrank, MPI_COMM_WORLD);
-   /*printf("A: rank is %d:\n",myrank);
-   for(int i=0;i<squareSize*squareSize;i++){
-       printf("%lf ", a[i]);
-   }*/
-   //printf("\n");
-   printf("\nA: rank is %d:\n",myrank);
-   for(int i=0;i<squareSize*squareSize;i++){
-       printf("%lf ", a[i]);
-   }
-   printf("\nB: rank is %d:\n",myrank);
-   for(int i=0;i<squareSize*squareSize;i++){
-       printf("%lf ", b[i]);
-   }
-   printf("\n");
+
    MPI_Barrier(MPI_COMM_WORLD);
    if(myrank==0)
      start = MPI_Wtime();
 
-   int colour = myrank/sqrtp;
-   //printf("colour for processor %d is %d\n", myrank,colour);
+   MPI_Comm_split(MPI_COMM_WORLD, myrank/sqrtp, myrank, &rowComm);
+   MPI_Comm_split(MPI_COMM_WORLD, myrank%sqrtp, myrank, &colComm);
 
-   MPI_Comm_split(MPI_COMM_WORLD, colour, myrank, &rowComm);
-   MPI_Comm_split(MPI_COMM_WORLD,myrank%sqrtp,myrank,&colComm);
-
+   // get other rows and columns
    // TODO: merge into one loop?
    for(i=0; i<sqrtp; i++){
        MPI_Gather(a, squareSize*squareSize, MPI_DOUBLE, rowA, (n*n)/p, MPI_DOUBLE, i, rowComm);
@@ -106,24 +103,12 @@ int main(int argc, char **argv) {
        MPI_Gather(b, squareSize*squareSize, MPI_DOUBLE, colB, (n*n)/p, MPI_DOUBLE, i, colComm);
    }
 
-   /*for(int l=0;l<squareSize*n;l++){
-       printf("rank is %d, element %d of rowA is %lf \n",myrank,l,rowA[l]);
-       //if(l=n*n-1){
-         //printf("\n");
-       //}
-   }
-   for(int l=0;l<squareSize*n;l++){
-       printf("rank is %d, element %d of colB is %lf \n",myrank,l,colB[l]);
-   }*/
-
-   /*for(int l=0;l<squareSize*n;l++){
-      printf("%lf ",colB[l]);
-   }*/
-
-
+   // do the actual multiplication
    for(i=0; i<squareSize; i++){
        for(j=0; j<squareSize; j++) {
            sum = 0.;
+           // introduce another loop for a bit of mathematical gymnastics
+           // as the blocks are gathered such that the original rows/columns of the matrices are not in order
            for(int l=0;l<sqrtp;l++){
                for(k=0; k<squareSize; k++){
                    sum += rowA[i*squareSize+k+(l*squareSize*squareSize)]*colB[k*squareSize+j+(l*squareSize*squareSize)];
@@ -136,12 +121,31 @@ int main(int argc, char **argv) {
    free(rowA);
    free(colB);
    MPI_Barrier(MPI_COMM_WORLD);
-   /*if(myrank==0)
+   if(myrank==0)
        printf("It took %f seconds to multiply 2 %dx%d matrices.\n",
-   MPI_Wtime()-start, n, n);*/
+   MPI_Wtime()-start, n, n);
    if(myrank==0)
      allC = malloc(n*n*sizeof(double));
-   MPI_Gather(c, squareSize*squareSize, MPI_DOUBLE, allC, squareSize*squareSize, MPI_DOUBLE,0, MPI_COMM_WORLD);
+
+   MPI_Request sendRequestC;
+   // processor with rank 0 should receive data from all other processors
+   for(int i=0;i<squareSize;i++){
+       MPI_Issend(&c[i*squareSize],squareSize,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&sendRequestC);
+   }
+
+   if(myrank==0){
+       int currentProc=0;
+       for(int i=0;i<sqrtp;i++){
+           for(int j=0;j<sqrtp;j++){
+               for(int k=0;k<squareSize;k++){
+                   MPI_Recv(&allC[(i*squareSize*n)+(j*squareSize)+(k*n)],squareSize,MPI_DOUBLE,currentProc,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+               }
+               currentProc++;
+           }
+       }
+   }
+   MPI_Wait(&sendRequestC, MPI_STATUS_IGNORE);
+   //MPI_Gather(c, squareSize*squareSize, MPI_DOUBLE, allC, squareSize*squareSize, MPI_DOUBLE,0, MPI_COMM_WORLD);
    if(myrank==0) {
      for(i=0, sumdiag=0.; i<n; i++)
        sumdiag += allC[i*n+i];
@@ -153,11 +157,31 @@ int main(int argc, char **argv) {
         printf("\n");
     }
    }
-   if(myrank==0)
+   if(myrank==0){
      free(allC);
+     free(matA);
+     free(matB);
+   }
    MPI_Finalize();
    free(a);
    free(b);
    free(c);
 }
 
+void InitializeMatrix(int seed, double *matrix){
+    srand(time(NULL));    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            matrix[i*n + j] = ((rand()%10+1) * seed);
+        }
+    }
+}
+
+void PrintMatrix(double *matrixToPrint){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%lf ", matrixToPrint[i*n + j]);
+        }
+        printf("\n");
+    }
+}
